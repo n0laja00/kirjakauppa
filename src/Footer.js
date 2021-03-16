cd43e2bbe61e4e691a249b0074b05976bba6b6ce
Aleksi Hannula <n9haal00@students.oamk.fi>
2021-03-16 11:06:18 +0200

    Footer värimaailma

--- src/Footer.js
+++ src/Footer.js
@@ -2,61 +2,74 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="row mt-5">
-          <div className="col-4">
-            <p>Lorem impsum lorem ipsum lorem ipsum</p>
-          </div>
-          <div className="col-4">
-          <div class="container mt-5 mb-5 d-flex justify-content-center">
-              <ul class="social-list">
-                  <li>
-                      <div class="maincard">
-                          <div class="thecard">
-                            <a href="#">
-                              <div class="thefront text-center facebook">
-                                  <div class="social-icon"> <i class="fa fa-facebook fa-2x"></i> </div>
-                              </div>
-                              <div class="theback text-center facebook">
-                                  <div class="social-text mt-1"> <small class="font-weight-bold">Facebook</small> </div>
-                              </div>
-                            </a>
-                          </div>
-                      </div>
-                  </li>
-                  <li>
-                      <div class="maincard">
-                          <div class="thecard">
-                            <a href="#">
-                              <div class="thefront text-center instagram">
-                                  <div class="social-icon"> <i class="fa fa-instagram fa-2x"></i> </div>
-                              </div>
-                              <div class="theback text-center instagram">
-                                  <div class="social-text mt-1"> <small class="font-weight-bold">Instagram</small> </div>
-                              </div>
-                            </a>
-                          </div>
-                      </div>
-                  </li>
-                  <li>
-                      <div class="maincard ">
-                          <div class="thecard">
-                            <a href="#">
-                              <div class="thefront text-center youtube">
-                                  <div class="social-icon"> <i class="fa fa-youtube fa-2x"></i> </div>
-                              </div>
-                              <div class="theback text-center youtube">
-                                  <div class="social-text mt-1"> <small class="font-weight-bold">Youtube</small> </div>
-                              </div>
-                            </a>
-                          </div>
-                      </div>
-                  </li>
-              </ul>
-          </div>
-          </div>
-          <div className="col-4">
-            <p>Lorem impsum lorem ipsum lorem ipsum</p>
-          </div>
+        <footer className="mt-5">
+            <section className="row text-center fw-bold">
+                <div className="col-4 border-dark border footer_top pt-2">
+                    <p>Sijainti</p>
+                </div>
+                <div className="col-4 border-dark border footer_top pt-2">
+                    <p>Somet</p>
+                </div>
+                <div className="col-4 border-dark border footer_top pt-2">
+                    <p>Yhteystiedot</p>
+                </div>
+            </section>
+            <section className="row border border-dark">
+            <div className="col-4">
+                <p>Lorem impsum lorem ipsum lorem ipsum</p>
+            </div>
+            <div className="col-4 border-dark border">
+                <div class="container mt-5 mb-5 d-flex justify-content-center">
+                    <ul class="social-list">
+                        <li>
+                            <div class="maincard">
+                                <div class="thecard">
+                                    <a href="#">
+                                    <div class="thefront text-center facebook">
+                                        <div class="social-icon"> <i class="fa fa-facebook fa-2x"></i> </div>
+                                    </div>
+                                    <div class="theback text-center facebook">
+                                        <div class="social-text mt-1"> <small class="font-weight-bold">Facebook</small> </div>
+                                    </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="maincard">
+                                <div class="thecard">
+                                    <a href="#">
+                                    <div class="thefront text-center instagram">
+                                        <div class="social-icon"> <i class="fa fa-instagram fa-2x"></i> </div>
+                                    </div>
+                                    <div class="theback text-center instagram">
+                                        <div class="social-text mt-1"> <small class="font-weight-bold">Instagram</small> </div>
+                                    </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="maincard ">
+                                <div class="thecard">
+                                    <a href="#">
+                                    <div class="thefront text-center youtube">
+                                        <div class="social-icon"> <i class="fa fa-youtube fa-2x"></i> </div>
+                                    </div>
+                                    <div class="theback text-center youtube">
+                                        <div class="social-text mt-1"> <small class="font-weight-bold">Youtube</small> </div>
+                                    </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="col-4">
+                <p>Lorem impsum lorem ipsum lorem ipsum</p>
+            </div>
+            </section>
     </footer>
     )
 }
