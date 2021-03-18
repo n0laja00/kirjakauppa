e8e2362070c717cc768a34403cb615a38d85321a
unknown <n0laja00@students.oamk.fi>
2021-03-18 11:04:46 +0200

    fontawersome

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Navbar() {
     return (
         <div className="col-sm-12 navbar">
-            <h3>Navbar här</h3>
+            <i className="fa fa-book">fakta</i>
+            <i className="fa fa-exclamation">toiminta</i> 
+            <i className="fa fa-magic">Sci-fi ja Fantasia</i>
+            <i className="fa fa-mortar-board">Oppikirjat</i>
+            <i className="fa fa-secret-user">Kauhu ja Trilleri</i>
+            <i className="fa fa-heart">Romantiikka</i>
+            <i className="fa fa-map-pin">Lastenkirjat</i>
+
         </div>
     )
 }
