7ea17f59e2f1a39ef9e277ed9d0f368c65edd1f3
Riku Pitkanen <n0piri00@students.oamk.fi>
2021-03-18 11:01:58 +0200

    header.js ja navbar semi-ok

--- src/Header.js
+++ src/Header.js
@@ -5,13 +5,13 @@ export default function Header() {
     return (
         <div className="row header">
             <div className="row">
-            <div className="col-sm-1">Logo?</div>
-            <div className="col-sm-10">
-            <h1 className="text-center">Satun Satumaailma</h1>
+                <div className="col-sm-1">Logo?</div>
+                <div className="col-sm-10">
+                    <h1 className="text-center">Satun Satumaailma</h1>
                 </div>
-            <div className="shopcart_cs col-sm-1">Shopping cart
+                <div className="shopcart_cs col-sm-1">Shopping cart
             <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-            </div>
+                </div>
             </div>
             <Navbar />
         </div>
