094b4138b46617026058e8e07feae0c5a4ea3f53
Riku Pitkanen <n0piri00@students.oamk.fi>
2021-03-31 09:25:24 +0300

    kaikkien kirjojen renderöinti ja bookDiv-tyylimäärittelyt

--- src/AllBooks.js
+++ src/AllBooks.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useState, useEffect, React } from 'react'
+
 
 export default function AllBooks() {
+
+    const [books, setBooks] = useState([]);
+    const [error, setError] = useState('');
+    const URL = 'http://localhost/projekti/kaikkiKirjat.php';
+    const imgURL = 'http://localhost/projekti/img/'
+
+    useEffect(() => {
+        fetch(URL)
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    setBooks(result);
+                    console.log(result);
+                    // setIsLoaded(true);
+                    // setItems(result.articles);
+                }, (error) => {
+                    setError(error);
+                    console.log(error);
+                    console.log(books)
+                    // setIsLoaded(true);
+                    // setItems([]);
+                }
+            )
+    }, [])
+
+
     return (
-        <div>
-            <h1>Kaikki kirjat</h1>
+        <div className="row">
+            <div className="col-12 text-center py-4">
+                <h1>Kaikki kirjat</h1>
+            </div>
+            <div className="row d-flex justify-content-center">
+                {books.map(book => (
+                    <div className="col-5 bookDiv"><img src="{imgURL}+{book.kuva}" /* alt={book.kirjaNimi} */ className="img-fluid"></img>
+                    {book.kirjaNimi}
+                    <div>{book.kuvaus}</div>
+                    <div>Sivumäärä: {book.sivuNro}</div>
+                        <div>Julkaisupäivä: {book.julkaistu}</div>
+                        <div><b>Hinta: {book.hinta}</b></div>
+                    </div>
+
+                ))}
+            </div>
         </div>
     )
 }
