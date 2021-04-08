d4ebb42a8219dd9b1d8bf6ba962480a690133ddb
Riku Pitkanen <n0piri00@students.oamk.fi>
2021-04-08 12:49:16 +0300

    bookdetails layoutin suunnittelua

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -9,6 +9,8 @@ export default function BookDetails() {
     const [book, setBook] = useState([]);
     const [error, setError] = useState('');
     const URL = 'http://localhost/kirjakauppa/haeKirjaNro.php/'
+    const imgURL = 'http://localhost/kirjakauppa/img/';
+
 
     useEffect(() => {
         fetch(URL + '/' + id)
@@ -23,15 +25,30 @@ export default function BookDetails() {
     }, [])
 
     return (
-        <div className="row">
+        <>
             {book.map(el => (
-                <div className="col-12 text-center">
-                <h1>{el.kirjaNimi}</h1>
-                <div>{el.kuvaus}</div>
-                <div>{el.hinta}</div>
+                // poista tästä bg-warning ku valmis
+                <div className="row">
+                    <div className="row">
+                        <h1 className="col-sm-12">{el.kirjaNimi}</h1>
+                        <div className="col-sm-12">{el.etunimi} {el.sukunimi}</div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <img class="card-img-top" src={imgURL + el.kuva} alt={el.kirjaNimi} />
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="row customBorder bottomBg p-3">
+                                <h3 className="col-sm-12">Tuotekuvaus</h3>
+                                <div className="col-sm-12">{el.kuvaus}</div>
+                                <div className="col-sm-6">Hinta: {el.hinta} €</div>
+                                <button className="btn btn-primary m-3 col-8">Ostoskoriin</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             ))}
-        </div>
+        </>
     )
 
 }
