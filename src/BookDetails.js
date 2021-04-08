345c1a749e342e3d4bd1c37950a85103df1d873a
Riku Pitkanen <n0piri00@students.oamk.fi>
2021-04-08 16:04:44 +0300

    luotu Reviews.js

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,5 +1,6 @@
 import { React, useState, useEffect } from 'react'
 import { useParams } from 'react-router';
+import Reviews from './Reviews';
 
 export default function BookDetails() {
 
@@ -63,6 +64,7 @@ export default function BookDetails() {
                         </div>
                     </div>
                 ))}
+                <Reviews />
             </>
         )
     }
