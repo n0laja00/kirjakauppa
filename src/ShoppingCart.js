89bdc47c797a282ccd3e2bd6b0be28d8a042049d
Janne Lammela <n0laja00@students.oamk.fi>
2021-04-19 18:41:05 +0300

    ostoskorista pois korinappi

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CartList from './cartComponents/CartList'
 import TestAddItem from './cartComponents/TestAddItem'
 import ToRegistry from './cartComponents/ToRegistry'
@@ -7,14 +7,24 @@ import CartContextProvider from './contexts/CartContext'
 
 
 const ShoppingCart = () => {
+    let localState = localStorage.getItem('cart').length;
+    const [cartState, setCartState] = useState(false)
+    
+
+    useEffect(() => {
+        if(localState >= 3) {setCartState(false);}
+        else{setCartState(true)};
+        localState = localStorage.getItem('cart').length;
+    },[])
+
     return ( 
         
         <div className="row mt-3 py-4 d-flex justify-content-center cart-page">
             <div>
                 <CartContextProvider>
                     <CartList/> 
+                    
                 </CartContextProvider>
-                <ToRegistry/>
             </div>
         </div> 
         
